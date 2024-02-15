c82c2ebd2748dee7bea1c64f3591cb18ece0d0bb
Darkness2758 <basur0521@gmail.com>
2024-02-15 11:55:58 +0530

    working

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import json
+import os
 
 app = Flask(__name__)
 
@@ -7,49 +11,94 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-@app.route('/register', methods=['POST'])
-def register():
+@app.route('/submit', methods=['POST'])
+def submit():
     if request.method == 'POST':
-        name = request.form['name']
+        name = request.form['Name']
         email = request.form['email']
         phone = request.form['phone']
         address = request.form['address']
-        food_type = request.form['foodType']
         quantity = request.form['quantity']
 
         if not (name and email and phone and address and quantity):
-            return 'All fields are required'
-
+            return "All fields are required"
+        #Data to be fetched and stored
         registration_data = {
             'name': name,
             'email': email,
             'phone': phone,
             'address': address,
-            'food_type': food_type,
             'quantity': quantity
         }
 
-        try:
-            with open('registrations.json', 'r') as file:
-                registrations = json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            registrations = []
+        save_registration(registration_data)
+        
+        #email msg 
 
-        registrations.append(registration_data)
+        summary_message = f"""We are delighted to inform you that your registration for food donation at Zero Hunger was successful!\n {name} Your commitment to contributing to our cause is truly appreciated, and we are thrilled to have you join our efforts in fighting hunger and making a positive impact in our community\n"""
+        summary_message += f"Details:\n"
+        summary_message += f"Email: {email}\n"
+        summary_message += f"Phone: {phone}\n"
+        summary_message += f"Address: {address}\n"
+        summary_message += f"Quantity: {quantity} kg\n"
 
-        with open('registrations.json', 'w') as file:
-            json.dump(registrations, file, indent=4)
+        send_email(email, summary_message)
 
-        return redirect(url_for('success'))
+        return summary_message
+    
+def save_registration(data):
+    try:
+        with open('data/registrations.json', 'r') as file: #Form Details are being stored in a json file
+            registrations = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        registrations = []
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
+    registrations.append(data)
 
+    with open('data/registrations.json', 'w') as file:
+        json.dump(registrations, file, indent=4)
+
+def send_email(receiver_email, message):
+    sender_email = "zhunger7@gmail.com" #leave this things like this 
+    password = "kljm aqow swdu hssj" 
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = "Registration Success"
+
+    msg.attach(MIMEText(message, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)  #smpt gmail server and pass key
+    server.starttls()
+    server.login(sender_email, password) 
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.quit()
+
+#routing html pages with python flask server
+  
 @app.route('/food_donation')
 def food_donation():
-    return render_template('fd.html')
+    return render_template('fd.html') 
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/hotel_info')
+def hotel_info():
+    return render_template('hotel.html')
+
+@app.route('/area')
+def area():
+    return render_template('area.html')
+
+@app.route('/location')
+def location():
+    return render_template('location.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
